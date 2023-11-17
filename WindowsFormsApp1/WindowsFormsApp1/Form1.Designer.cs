4f5ab32f12ebf9b8bcda692decd9fb7a0a19e11a
Asared <117143510+Asared@users.noreply.github.com>
2023-11-17 06:48:05 +0300

    1

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Blue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Gold;
+            this.button1.Location = new System.Drawing.Point(467, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Войти";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(467, 301);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightGreen;
+            this.textBox1.Location = new System.Drawing.Point(467, 238);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(290, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.LightGreen;
+            this.textBox2.Location = new System.Drawing.Point(467, 278);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(290, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources._1212;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1403, 783);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(503, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(196, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Введите логин и пароль";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(-1, 750);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(36, 31);
+            this.button2.TabIndex = 7;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Blue;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.Color.Gold;
+            this.button3.Location = new System.Drawing.Point(548, 316);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(209, 36);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Зарегистрироваться";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.LightGreen;
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(467, 316);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.PasswordChar = '*';
+            this.textBox3.Size = new System.Drawing.Size(290, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(467, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Введите логин";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(467, 262);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Введите пароль";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Enabled = false;
+            this.label5.Location = new System.Drawing.Point(467, 300);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Повторите пароль";
+            this.label5.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.Enabled = false;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.ForeColor = System.Drawing.Color.Yellow;
+            this.button4.Location = new System.Drawing.Point(356, 175);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(536, 345);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Вы успешно зарегистрировались";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orange;
+            this.ClientSize = new System.Drawing.Size(1390, 781);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Numerics;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            shag(22);
+        }
+        private bool _isLogIn = true;
+        private bool _isSignUp = false;
+        class User
+        {
+            private string _username;
+            private string _password;
+            public User(string username, string password)
+            {
+                _username = username;
+                _password = password;
+            }
+        }
+        public void shag(int sh)
+        {
+            if (sh < 0)
+                sh = (sh * 2 + characters.Length) / 2;
+            int z = 0, c = sh, max;
+            max = characters.Length / (sh + 1);
+            max = max * (sh + 1);
+            for (int i = 0; i < max; i++)
+            {
+
+                characters1 = characters1 + characters[sh - z];
+                Console.Write(characters[sh - z] + ",");
+                z++;
+                if (c + 1 == z)
+                {
+                    sh = sh + c + 1;
+                    z = 0;
+                }
+            }
+            for (int i = characters.Length - 1; i > max - 1; i--)
+            {
+                characters1 = characters1 + characters[i];
+            }
+        }
+        string characters = "#IJKQRSTUcdefABCDEFGHghijklmnopqVWXYZabrstuvwxyz 12345LMNOP67890";
+        string characters1 = "";
+        private string Chiper(string text) 
+        {
+            string ciphertext = "";
+
+            for (int i = 0; i < text.Length; i++)
+                for (int j = 0; j < characters.Length + 1; j++)
+                {
+                    if (j == characters.Length)
+                    {
+                        ciphertext = ciphertext + text[i];
+                        break;
+                    }
+                    if (text[i] == characters[j])
+                    {
+                        ciphertext = ciphertext + characters1[j];
+                        break;
+                    }
+                }
+            return ciphertext;
+        }
+        private void WriteFile(string username, string password)
+        {
+            string path = @"D:\visual_projects\WindowsFormsApp1\WindowsFormsApp1\Resources\1.txt";   // путь к файлу
+
+            string text = username+"\n"+password; // строка для записи
+
+            // запись в файл
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                writer.WriteLineAsync(Chiper(text));
+            }
+        }
+        private string DeChiper(string text)
+        {
+            string plaintext = "";
+
+            for (int i = 0; i < text.Length; i++)
+                for (int j = 0; j < characters1.Length + 1; j++)
+                {
+                    if (j == characters1.Length)
+                    {
+                        plaintext = plaintext + text[i];
+                        break;
+                    }
+                    if (text[i] == characters1[j])
+                    {
+                        plaintext = plaintext + characters[j];
+                        break;
+                    }
+                }
+
+
+            return plaintext;
+        }
+        private bool IsFound(string username,string password)
+        {
+            string path = @"D:\visual_projects\WindowsFormsApp1\WindowsFormsApp1\Resources\1.txt";   // путь к файлу
+            bool isFound=false;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line, line2;
+                while ((line =reader.ReadLine()) != null && (line2 = reader.ReadLine()) != null)
+                {
+                    if(username== DeChiper(line) && password== DeChiper(line2))
+                    {
+                        isFound = true;
+                    }
+                }
+            }
+            return isFound;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_isLogIn)
+            {
+                if (IsFound(textBox1.Text,textBox2.Text))
+                {
+                    button1.Enabled = false;
+                    button1.Visible = false;
+                    textBox1.Enabled = false;
+                    textBox1.Visible = false;
+                    textBox2.Enabled = false;
+                    textBox2.Visible = false;
+                    label1.Enabled = false;
+                    label1.Visible = false;
+                    label2.Enabled = false;
+                    label2.Visible = false;
+                    pictureBox1.Enabled = true;
+                    pictureBox1.Visible = true;
+                    button3.Visible = false;
+                    button3.Enabled=false;
+                    label3.Enabled = false;
+                    label3.Visible = false;
+                    label4.Enabled = false;
+                    label4.Visible = false;
+                    label5.Enabled = false;
+                    label5.Visible = false;
+                }
+                else
+                {
+                    label1.Text = "Неправильно введен логин или пароль";
+                }
+            }
+            else 
+            {
+                _isLogIn = true;
+                _isSignUp = false;
+                textBox3.Enabled = false;
+                textBox3.Visible = false;
+                label5.Visible = false;
+                label5.Enabled = false;
+                button3.Location = new Point(548, 315);
+                button1.Location = new Point(467, 315);
+                label1.Location = new Point(467, 300);
+                label1.Enabled = false;
+                label1.Visible = false;
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+            }
+        }
+        private bool IsStrong(string password)
+        {
+            bool isStrong = true;
+            string up =   "IJKQRSTUABCDEFGHVWXYZLMNOP";
+            string down = "cdefghijklmnopqabrstuvwxyz";
+            string numbers = "1234567890";
+            int t = 0;
+            
+            for (int i = 0;i<password.Length;i++)
+            {
+                for (int j = 0;j<up.Length;j++)
+                {
+                    if (password[i] == up[j])
+                    {
+                        t++;
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i < password.Length; i++)
+            {
+                for (int j = 0; j < down.Length; j++)
+                {
+                    if (password[i] == down[j])
+                    {
+                        t++;
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i < password.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (password[i] == numbers[j])
+                    {
+                        t++;
+                        break;
+                    }
+                }
+            }
+            if(t<password.Length)
+            {
+                isStrong = false;
+            }    
+            if (password.Length < 8)
+            {
+                isStrong = false;
+            }
+            return isStrong;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (_isSignUp)
+            {
+                if (textBox2.Text == textBox3.Text)
+                {
+                    if (IsStrong(textBox2.Text))
+                    {
+                        WriteFile(textBox1.Text, textBox2.Text);
+                        _isLogIn = true;
+                        _isSignUp = false;
+                        textBox3.Enabled = false;
+                        textBox3.Visible = false;
+                        label5.Visible = false;
+                        label5.Enabled = false;
+                        button3.Location = new Point(548, 315);
+                        button1.Location = new Point(467, 315);
+                        label1.Location = new Point(467, 300);
+                        label1.Enabled = false;
+                        label1.Visible = false;
+                        textBox1.Text = "";
+                        textBox2.Text = "";
+                        button4.Enabled = true;
+                        button4.Visible = true;
+                    }
+                    else
+                    {
+                        label1.Text = "Пароль должен содеражать не менее 8 символов, заглавную букву, прописную букву,цифру";
+                    }
+                }
+                else
+                {
+                    label1.Text = "Пароли не совпадают";
+                }
+            }
+            else
+            {
+                _isLogIn = false;
+                _isSignUp = true;
+                textBox3.Enabled = true;
+                textBox3.Visible = true;
+                label5.Visible = true;
+                label5.Enabled = true;
+                button3.Location = new Point(506, 354);
+                button1.Location = new Point(564, 504);
+                label1.Location = new Point(467, 339);
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button4.Enabled = false;
+            button4.Visible = false;
+        }
+    }
+}
